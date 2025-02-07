c26c75d45bac42e076a0b84795d837ad4145c673
Doughtry <chas_doughtry1@baylor.edu>
2025-02-07 09:12:50 -0600

    Added cost page/logic

---
+++ client/src/components/header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="header">
+           <h1>Meals To You Header</h1>
+        </header>
+    );
+}
+
+export default Header;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from '../components/header.js';
 
 const Home = () => {
   return (
     <div>
+        <Header/>
       <h1>This is the homepage</h1>
     </div>
   );
