4614d282d05870480328c49fe970e3f4ac4b2cdc
SecondPretender <peterm03cg@gmail.com>
2025-02-15 21:31:52 -0600

    began login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,18 @@
+
+import React, { useState } from "react";
+import Header from '../components/header.js';
+import "../styles/header.css";
+
+const Login = () => {
+
+
+    return (
+        <div>
+            <main>
+                Hi
+            </main>
+        </div>
+    );
+};
+
+export default Login;
