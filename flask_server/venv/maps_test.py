9660d1935f1bab9c2bc4d9d9bb10f7bb28c18a31
Isaac Marsh <imarsh649@gmail.com>
2025-03-16 18:48:49 -0500

    Remove file

--- flask_server/venv/maps_test.py
+++ flask_server/venv/maps_test.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 
-from flask_server.db import connect_db, test_db
+
 from flask_server.maps import get_osrm_route
 
 
@@ -22,10 +22,7 @@ class MyTestCase(unittest.TestCase):
 
         print(get_osrm_route(coordinates))
 
-    def test_db(self):
-        connect_db()
-        test_db()
-        # assert 1 == 2
+
 
 
 if __name__ == '__main__':
