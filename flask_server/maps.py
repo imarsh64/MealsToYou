c90b10a6e7aaa21035256a02fc0536f330bb0741
Isaac Marsh <imarsh649@gmail.com>
2025-03-16 18:43:12 -0500

    Travelling sales man

--- flask_server/maps.py
+++ flask_server/maps.py
@@ -1,32 +1,9 @@
-# Optimal path
-import requests
-import json
-
-OSRM_URL = "http://router.project-osrm.org/route/v1/driving/"
-
-"""
-locations is a list of lists (list of coordinate pairs)
-"""
-def get_osrm_route(locations):
-    if len(locations) < 2:
-        print("Skipping OSRM request: Route must have at least 2 points.")
-        return None
-
-    coord_string = ";".join([f"{lon},{lat}" for lat, lon in locations])
-    response = requests.get(f"{OSRM_URL}{coord_string}?overview=full&geometries=geojson")
-
-    if response.status_code == 200 and "routes" in response.json() and response.json()["routes"]:
-        print(json.dumps(response.json(), indent=4))
-        return [waypoint["location"] for waypoint in response.json()["waypoints"]]
-        # return response.json()["routes"][0]["geometry"]["coordinates"]
-    else:
-        print(f"OSRM request failed: {response.status_code}")
-        return None
 
 
 # Generate google maps link
 def generate_google_maps_link(*addresses):
     google_maps_link = 'https://www.google.com/maps/dir/'
     # formatted_addresses = [addr.replace(" ", "+") for addr in addresses]
-    formatted_coords = [f"{lat},{lon}" for lon, lat in addresses]
+    # formatted_coords = [f"{lat},{lon}" for lon, lat in addresses]
+    formatted_coords = [f"{lat},{lon}" for lat, lon in addresses]
     return google_maps_link + "/".join(formatted_coords)
--- flask_server/server.py
+++ flask_server/server.py
@@ -3,6 +3,7 @@ import mysql.connector
 from flask import Flask
 
 from flask_server.maps import get_osrm_route, generate_google_maps_link
+from flask_server.tsp import make_distance_matrix, genetic_tsp
 
 app = Flask(__name__)
 
@@ -22,11 +23,15 @@ def route(district):
     cursor = mydb.cursor()
     query = "SELECT latitude, longitude FROM address WHERE district = %s"
     params = district
+
     cursor.execute(query, (params,))
     addresses = cursor.fetchall()
-    # address_flipped = [tuple(reversed(tup)) for tup in addresses]
-    ordered = get_osrm_route(addresses)
-    return generate_google_maps_link(*ordered) # generate_google_maps_link(*[list(tup) for tup in address_flipped])
+    addresses = [(float(row[0]), float(row[1])) for row in addresses]
+
+    tsp = genetic_tsp(addresses)
+
+    ordered = [addresses[i] for i in tsp]
+    return generate_google_maps_link(*ordered)
 
 
 if __name__ == "__main__":
